4b1e5e09d39933ac86f73ddd132ddc7e5962a7a1
Shaghayegh Rahmani <74653444+rezvan-net@users.noreply.github.com>
2021-06-17 16:54:53 +0430

    Add files via upload

---
+++ voice command.py
@@ -0,0 +1,10 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+
+engine.say ('Hi, This is shaghayegh Rahmani')
+engine.say(' I love you my dear followers')
+engine.say ('Happy 2 k Followers in Instagram')
+engine.say ('My instagram page is rezvan_net')
+
+engine.runAndWait()
